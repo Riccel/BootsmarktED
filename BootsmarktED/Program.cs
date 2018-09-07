ce493d1d30d19405c929808487ced1707d4a3232
Riccel <daniel.wolff2@web.de>
2018-09-07 18:31:28 +0200

    Programm initialisieren

---
+++ BootsmarktED/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootsmarktED
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
